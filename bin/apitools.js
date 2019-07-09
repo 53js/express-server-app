23be2f0ee356af6dfa7c187ad28b7c5b7a3cc48e
Hugo Mallet <hugo.mallet@53js.fr>
2019-07-09 15:43:48 +0200

    refactor: improved bin script

--- bin/apitools.js
+++ bin/apitools.js
@@ -1,43 +1,23 @@
 #!/usr/bin/env node
 
-const { exec } = require('child_process');
-const { join } = require('path');
-const program = require('commander');
+const execa = require('execa');
 
-const pkg = require('../package');
+const LOCAL_PACKAGES = '..';
 
-process.on('unhandledRejection', (err) => {
-	throw err;
-});
+const [cmd, ...args] = process.argv.slice(2);
+const argsStr = args.join(' ').trim();
 
-const crossEnv = join(__dirname, '../node_modules/.bin/cross-env');
-const jest = join(__dirname, '../node_modules/.bin/jest');
-const nodemon = join(__dirname, '../node_modules/.bin/nodemon');
-const pinoPretty = join(__dirname, '../node_modules/.bin/pino-pretty');
+const exec = command => execa.commandSync(
+	command,
+	{ preferLocal: LOCAL_PACKAGES, shell: true, stdio: 'inherit' },
+);
 
-const execute = async (...args) => {
-	const { stderr, stdin, stdout } = await exec(...args);
-	stderr.pipe(process.stderr);
-	stdout.pipe(process.stdout);
-	process.stdin.pipe(stdin);
+const start = () => exec(`nodemon --inspect-brk ${argsStr} | pino-pretty -c -t`);
+const test = () => exec(`cross-env NODE_ENV=test jest --coverage --watchAll ${argsStr}`);
+
+const commands = {
+	start,
+	test,
 };
 
-const start = (file) => {
-	execute(`${nodemon} --inspect-brk ${file} | ${pinoPretty} -c -t`);
-};
-
-const test = async (args) => {
-	execute(`${crossEnv} NODE_ENV=test ${jest} --watchAll ${args}`);
-};
-
-program.version(pkg.version);
-
-program
-	.command('start <file>')
-	.action(start);
-
-program
-	.command('test')
-	.action(test);
-
-program.parse(process.argv);
+commands[cmd]();
